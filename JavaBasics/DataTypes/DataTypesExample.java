1bccf8eeaae7b5bfb65e4f955011059310c0bf31
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-23 19:46:06 +0530

    add DataType

---
+++ JavaBasics/DataTypes/DataTypesExample.java
@@ -0,0 +1,15 @@
+public class DataTypesExample {
+    public static void main(String args[]) {
+        int age = 20; // Interger Data Type
+        double salary = 500000.50; // Double Data Type
+        char grade = 'A'; // String Data Type
+        boolean isJavaFun = true; // Boolean Data Type
+
+        System.err.println(age);
+        System.err.println(salary);
+        System.err.println(grade);
+        System.err.println(isJavaFun);
+
+    }
+
+}
