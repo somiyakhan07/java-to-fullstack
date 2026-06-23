1849258b223f1593e3e449a46c5eb33553d0d9bc
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-23 18:08:29 +0530

    add variables in javabasics

---
+++ Java-Basics/Variables/VariableExample.java
@@ -0,0 +1,11 @@
+public class VariableExample {
+    public static void main(String args[]) {
+
+        int age = 20;
+        String name = "Somiya";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+    }
+}
