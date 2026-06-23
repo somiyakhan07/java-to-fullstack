61c872e8df7d877e6c7acc32e479b898665a8fb2
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-23 19:03:42 +0530

    Rename Java-Basic to JavaBasics

---
+++ JavaBasics/Variables/InstanceVariable.java
@@ -0,0 +1,11 @@
+public class InstanceVariable {
+
+    int age = 20;
+
+    public static void main(String args[]) {
+
+        InstanceVariable obj = new InstanceVariable();
+        System.out.println(obj.age);
+    }
+
+}
---
+++ JavaBasics/Variables/LocalVariable.java
@@ -0,0 +1,8 @@
+public class LocalVariable {
+    public static void main(String args[]){
+        int number = 10; // local variable
+
+        System.out.println(number);
+    }
+    
+}
