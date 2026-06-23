94ac3fb70e7e3988c727f3b1ce903c4c916ed84a
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-23 19:36:12 +0530

    delete Java-Basics

--- Java-Basics/Variables/LocalVariable.java
+++
@@ -1,8 +0,0 @@
-public class LocalVariable {
-    public static void main(String args[]){
-        int number = 10; // local variable
-
-        System.out.println(number);
-    }
-    
-}
--- Java-Basics/Variables/StaticVariable.java
+++
@@ -1,7 +0,0 @@
-public class StaticVariable {
-static String college = "ABC College";
-    public static void main(String args[]){
-System.out.println(college);
-    }
-    
-}
