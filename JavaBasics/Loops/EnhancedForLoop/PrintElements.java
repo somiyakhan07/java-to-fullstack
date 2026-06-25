78e9c827a59fde9f752de08760dbd226cb508eb3
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-25 18:40:00 +0530

    add EnhancedForLoop folder and PrintElementsexample.java too.

---
+++ JavaBasics/Loops/EnhancedForLoop/PrintElements.java
@@ -0,0 +1,9 @@
+public class PrintElements {
+    public static void main(String args[]) {
+        int nums[] = { 10, 20, 30, 40 };
+
+        for (int n : nums) {
+            System.out.println(n);
+        }
+    }
+}
