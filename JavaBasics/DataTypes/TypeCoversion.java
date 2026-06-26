c6d29765c0dda3d85f81a063eafdc0f6cba4f474
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-27 01:02:08 +0530

    add TypeCoversion.java in DataTypes

---
+++ JavaBasics/DataTypes/TypeCoversion.java
@@ -0,0 +1,33 @@
+//Type Conversion Examples
+
+public class TypeCoversion {
+    public static void main(String args[]) {
+        byte b = 10;
+        short s = b;
+
+        int i = s;
+
+        long l = i;
+
+        float f = l;
+
+        double d = f;
+
+        System.out.println("byte value: " + b);
+        System.out.println("short value: " + s);
+        System.out.println("int value: " + i);
+        System.out.println("long value: " + l);
+        System.out.println("float value: " + f);
+        System.out.println("double value: " + d);
+    }
+
+}
+
+/* O/P:
+byte value: 10
+short value: 10
+int value: 10
+long value: 10
+float value: 10.0
+double value: 10.0
+*/
