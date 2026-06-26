db5b38d5d6410ed2762b7155a41533c4afd350a3
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-27 00:25:37 +0530

    add comments in NonPrimitiveDataTypes.java

--- JavaBasics/DataTypes/NonPrimitiveDataTypes.java
+++ JavaBasics/DataTypes/NonPrimitiveDataTypes.java
@@ -1,28 +1,39 @@
-class Student {
+// Demonstrates examples of non-primitive data types like String, Array, and Object.
+
+class Student { // Create Class
     String name;
     int rollno;
 }
 
 public class NonPrimitiveDataTypes {
-    public static void main(String args[]){
-    String name = "somiya";
+    public static void main(String args[]) {
+        String name = "somiya"; // String
 
-    int nums[] = {10,20,30};
+        int nums[] = { 10, 20, 30 }; // array
 
-    Student s1 = new Student();
-    s1.name = "Aditi";
-    s1.rollno = 1;
+        Student s1 = new Student(); //create Student Object or referrence variable in s1
+        s1.name = "Aditi";
+        s1.rollno = 1;
 
-    System.out.println(name);
-    System.out.println();
+        System.out.println(name); // print string value
+        System.out.println();
 
-    for(int n : nums){
-        System.out.println("Elements of nums: " + n);
-    }
-    System.out.println();
+        for (int n : nums) {  // Print all array elements
+            System.out.println("Elements of nums: " + n);
+        }
+        System.out.println();
 
-    System.out.println("name- " + s1.name + " : " + s1.rollno);
+        System.out.println("name- " + s1.name + " : " + s1.rollno); // Print Student object details
 
     }
 
 }
+/*
+ * somiya
+ * 
+ * Elements of nums: 10
+ * Elements of nums: 20
+ * Elements of nums: 30
+ * 
+ * name- Aditi : 1
+ */
