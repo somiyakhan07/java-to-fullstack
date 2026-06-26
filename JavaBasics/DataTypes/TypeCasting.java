8f91a21db6ba5fffc7c21098037da7a6aadfa163
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-27 01:09:21 +0530

    add TypeCasting.java in Data Types

---
+++ JavaBasics/DataTypes/TypeCasting.java
@@ -0,0 +1,30 @@
+// Type Casting
+// Larger data type is manually converted into a smaller data type.
+
+public class TypeCasting {
+    public static void main(String args[]) {
+        double d = 99.99;
+        float f = (float) d;
+        long l = (long) f;
+        int i = (int) l;
+        short s = (short) i;
+        byte b = (byte) s;
+
+        System.out.println("double value: " + d);
+        System.out.println("float value: " + f);
+        System.out.println("long value: " + l);
+        System.out.println("int value: " + i);
+        System.out.println("short value: " + s);
+        System.out.println("byte value: " + b);
+    }
+
+}
+
+/*
+double value: 99.99
+float value: 99.99
+long value: 99
+int value: 99
+short value: 99
+byte value: 99
+ */
