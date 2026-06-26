fe555734a144bd3ac1d51bd9b52954cbeffc3be0
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-27 00:05:16 +0530

    add PrimitiveDataTypes.java in DataTypes

---
+++ JavaBasics/DataTypes/PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+public class PrimitiveDataTypes {
+    public static void main(String arg[]) {
+        byte b = 10;
+        short s = 100;
+        int i = 456;
+        long l = 1000000L;
+
+        float f = 10.5f;
+        double d = 20.55;
+
+        char ch = 'A';
+
+        boolean isJavaFun = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(isJavaFun);
+    }
+
+}
