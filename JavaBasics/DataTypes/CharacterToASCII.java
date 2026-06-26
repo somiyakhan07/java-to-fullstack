d2e502302e699316f1ef796558194bff2b4aac7b
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-27 01:39:09 +0530

    add CharacterToASCII.java in Data Types

---
+++ JavaBasics/DataTypes/CharacterToASCII.java
@@ -0,0 +1,22 @@
+// Demonstrates conversion of a character to its ASCII value.
+
+public class CharacterToASCII {
+    public static void main(String args[]) {
+        char ch = 'A'; // charater value
+
+        int ascii = ch;
+        // Java automatically converts the character to
+        // its corresponding ASCII(Unicode) integer value.
+
+        System.out.println(ch);
+        System.out.println(ascii);
+
+    }
+
+}
+
+/*
+O/P:
+A
+65
+ */
