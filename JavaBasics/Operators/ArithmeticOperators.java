62744e1be99b7d5debd783cddfeeeaa630e14612
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-28 23:40:48 +0530

    add ArithmeticOperators.java in Operators

---
+++ JavaBasics/Operators/ArithmeticOperators.java
@@ -0,0 +1,15 @@
+//Demonstrate Arithmetic Operators in Java
+
+class ArithmeticOperators {
+    public static void main(String args[]) {
+        int num1 = 4;
+        int num2 = 2;
+
+        System.out.println("Addition: " + (num1 + num2)); // addition operator
+        System.out.println("Subtraction: " + (num1 - num2)); // subtraction operator
+        System.out.println("Multiplication: " + (num1 * num2)); // multiplication operator
+        System.out.println("Division: " + (num1 / num2)); // division operator
+        System.out.println("Modulus: " + (num1 % num2)); // modulus operator
+
+    }
+}
