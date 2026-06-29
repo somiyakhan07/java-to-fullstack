d4e9db6933bd5967870e65210a435c1d3efc2de0
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-29 12:22:09 +0530

    Add Increment and Decrement Operators example in Operators

---
+++ JavaBasics/Operators/IncrementDecrementOperators.java
@@ -0,0 +1,27 @@
+// Demonstrate Increment and Decrement Operators
+
+public class IncrementDecrementOperator {
+    public static void main(String args[]) {
+        int num1 = 4;
+        num1++; // post-increment ++
+        System.out.println("After post-increment ++ of num1: " + num1);
+
+        int num2 = 5;
+        ++num2; // ++ pre-increment
+        System.out.println("After ++ pre-increment of num2: " + num2);
+
+        int num3 = 7;
+        num3--; // post-decrement --
+        System.out.println("After post-decrement -- of num3: " + num3);
+
+        int num4 = 9;
+        --num4; // -- pre-decrement
+        System.out.println("After -- pre-decrement: of num4: " + num4);
+    }
+}
+/*
+After post-increment ++ of num1: 5
+After ++ pre-increment of num2: 6
+After post-decrement -- of num3: 6
+After -- pre-decrement: of num4: 8
+ */
