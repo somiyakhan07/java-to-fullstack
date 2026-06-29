0ba999e954589f0e5d4f4c90090583c21a2c5422
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-29 12:35:42 +0530

    add RelationalOperators.java example in Operators

---
+++ JavaBasics/Operators/RelationalOperators.java
@@ -0,0 +1,25 @@
+// Demonstrate Relational Operators in java 
+
+public class RelationalOperators {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a == b: " + (a == b)); // Equal to operators
+        System.out.println("a != b: " + (a != b)); // Not equal to operator
+        System.out.println("a > b: " + (a > b)); // Greater than operator
+        System.out.println("a < b: " + (a < b)); // Less than operators
+        System.out.println("a >= b: " + (a >= b)); // Greater than or equal to operator
+        System.out.println("a <= b: " + (a <= b)); // Lesser than or equal to operator
+    }
+
+}
+
+/*
+ * false
+ * true
+ * false
+ * true
+ * false
+ * true
+ */
