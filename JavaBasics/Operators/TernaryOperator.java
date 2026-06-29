06ea1894012b96233c50a9d9082041bf7ded464c
somiyakhan07 <somiyakhan78600@gmail.com>
2026-06-29 13:01:25 +0530

    add TernaryOperator example in Operators

---
+++ JavaBasics/Operators/TernaryOperator.java
@@ -0,0 +1,15 @@
+// Demonstrates the ternary operator in Java.
+
+// Syntax: condition ? value_if_true : value_if_false
+
+public class TernaryOperator {
+    public static void main(String args[]) {
+        int n = 4;
+        int result = 0;
+        result = n % 2 == 0 ? 1 : 0; // Ternary Operator
+        System.out.println("n % 2 == 0 ? 1 : 0 = " + result);
+    }
+
+}
+
+// n % 2 == 0 ? 1 : 0 = 1
