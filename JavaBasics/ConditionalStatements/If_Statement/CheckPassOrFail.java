93f626ef69bb56ba020d88ad01eca33d475b0900
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-02 00:12:49 +0530

    add  CheckPassOrFail.java in If_statement

---
+++ JavaBasics/ConditionalStatements/If_Statement/CheckPassOrFail.java
@@ -0,0 +1,11 @@
+public class CheckPassOrFail {
+    public static void main(String[] args) {
+        int result = 90;
+
+        if (result >= 35) {
+            System.out.println("You are Pass with: " + result + " marks ");
+        }
+    }
+
+}
+// You are Pass with: 90 marks
