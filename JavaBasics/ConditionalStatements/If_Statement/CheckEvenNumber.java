8d7888efb3ab6128abbcd8bc95ad3bc380f6d079
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-02 00:07:09 +0530

    add CheckEvenNumber.java in If_statement

---
+++ JavaBasics/ConditionalStatements/If_Statement/CheckEvenNumber.java
@@ -0,0 +1,11 @@
+public class CheckEvenNumber {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is a even number");
+        }
+    }
+
+}
+// 10 is a even number
