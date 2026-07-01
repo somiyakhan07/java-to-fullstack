8a849856e6bc653698902e590a3a463c2b00357b
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-01 23:51:41 +0530

    add If_Statement/ with example in conditional statements

---
+++ JavaBasics/ConditionalStatements/If_Statement/CheckPositiveNumber.java
@@ -0,0 +1,11 @@
+public class CheckPositiveNumber{
+    public static void main(String[] args){
+        int num = 4;
+
+        if(num > 0){
+            System.out.println(num + " is a positive number");
+        }
+    }
+}
+
+// 4 is a positive number
