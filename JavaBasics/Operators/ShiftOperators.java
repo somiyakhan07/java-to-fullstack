f8adf521415ea97612b4c14ee6b5b0edfcd7f1e2
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-01 23:07:59 +0530

    add ShiftOperators.java example in Operators

---
+++ JavaBasics/Operators/ShiftOperators.java
@@ -0,0 +1,10 @@
+public class ShiftOperators {
+    public static void main(String[] args){
+        int num = 8;
+
+        System.out.println("num << 1 = " + (num << 1)); // Left Shift
+        System.out.println("num >> 1 = " + (num >> 1)); // Right Shift
+        System.out.println("num >>> 1 = " + (num >>> 1)); // Unsigned Right Shift
+    }
+    
+}
