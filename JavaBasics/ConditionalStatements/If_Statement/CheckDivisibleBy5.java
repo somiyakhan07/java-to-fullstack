f00c1c46b8e31350d5dd25c1e6c323c598adc791
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-03 20:12:34 +0530

    add  CheckDivisibleBy5.java in If_Statement

---
+++ JavaBasics/ConditionalStatements/If_Statement/CheckDivisibleBy5.java
@@ -0,0 +1,10 @@
+public class CheckDivisibleBy5 {
+    public static void main(String[] args) {
+        int num = 50;
+
+        if (num % 5 == 0) {
+            System.out.println(num + " is divisible by 5");
+        }
+    }
+}
+// 
