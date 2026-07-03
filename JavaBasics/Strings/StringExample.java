29cda8507b7c12cd56f336015b9c675576e56089
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-03 20:21:58 +0530

    add StringExample.java in Strig

---
+++ JavaBasics/Strings/StringExample.java
@@ -0,0 +1,7 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String name = "Somiya"; // String Declaration and initialization
+        System.out.println("name: " + name); // concatenation and printing
+    }
+}
+// Somiya
