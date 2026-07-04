8e7b769c3a5b1680c0470fe74d899bf911833430
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-05 00:24:18 +0530

    Add PracticeSet/ in Strings

---
+++ JavaBasics/Strings/PracticeSet/ReverseString.java
@@ -0,0 +1,13 @@
+// Reverse String using String Buffer
+
+public class ReverseString {
+    public static void main(String[] args){
+        StringBuffer prog = new StringBuffer ("Java Programming");
+
+        prog.reverse();
+        System.out.println(prog);
+    }
+    
+}
+
+// gnimmargorP avaJ
