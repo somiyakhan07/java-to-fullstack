26cb76c3f28432a4a17dd3475b196ecc9a5efa62
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-07 00:45:53 +0530

    add MonthNameUsingElseIf.java in else if ladder

---
+++ JavaBasics/ConditionalStatements/ElseIf_Ladder/MonthNameUsingElseIf.java
@@ -0,0 +1,37 @@
+// Print month name based on month number using Else-If Ladder
+
+public class MonthNameUsingElseIf {
+    public static void main(String[] args) {
+        int month = 7;
+
+        if (month == 1) {
+            System.out.println("january");
+        } else if (month == 2) {
+            System.out.println("february");
+        } else if (month == 3) {
+            System.out.println("march");
+        } else if (month == 4) {
+            System.out.println("april");
+        } else if (month == 5) {
+            System.out.println("may");
+        } else if (month == 6) {
+            System.out.println("june");
+        } else if (month == 7) {
+            System.out.println("july");
+        } else if (month == 8) {
+            System.out.println("august");
+        } else if (month == 9) {
+            System.out.println("september");
+        } else if (month == 10) {
+            System.out.println("october");
+        } else if (month == 11) {
+            System.out.println("november");
+        } else if (month == 12) {
+            System.out.println("december");
+        } else {
+            System.out.println("invalid input");
+        }
+    }
+
+}
+// july
