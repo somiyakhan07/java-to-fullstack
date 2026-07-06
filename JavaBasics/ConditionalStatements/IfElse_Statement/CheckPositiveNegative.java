3eddaf5b69ac9eabb560a71e3a4d44963cec7e7d
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-06 23:21:20 +0530

    add check positive negative example in ifelse statement

---
+++ JavaBasics/ConditionalStatements/IfElse_Statement/CheckPositiveNegative.java
@@ -0,0 +1,14 @@
+// Check Positive Negative
+
+public class CheckPositiveNegative{
+    public static void main(String[] args){
+        int num = 6;
+
+        if(num > 0){
+            System.out.println(num + " is Positive Number");
+        } else {
+             System.out.println(num + "is Negative Number");
+        }
+    }
+}
+// 6 is Positive Number
