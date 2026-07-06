8464c4c8f61f93c5f2a0174ba398f456519fe051
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-06 23:29:12 +0530

    add CheckLeapYear.java in If Else Statement

---
+++ JavaBasics/ConditionalStatements/IfElse_Statement/CheckLeapYear.java
@@ -0,0 +1,15 @@
+// Check Leap Year
+
+public class CheckLeapYear {
+    public static void main(String[] args) {
+        int year = 2026;
+
+        if (year % 4 == 0) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is not a Leap Year.");
+        }
+    }
+
+}
+// 2026 is not a Leap Year.
