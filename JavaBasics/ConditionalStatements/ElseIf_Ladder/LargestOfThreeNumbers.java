fbd0f34f5147c055a8f1c8552feb9437933ff770
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-07 00:15:33 +0530

    update largest of three numbers using else-if ladder

--- JavaBasics/ConditionalStatements/ElseIf_Ladder/LargestOfThreeNumbers.java
+++ JavaBasics/ConditionalStatements/ElseIf_Ladder/LargestOfThreeNumbers.java
@@ -6,9 +6,9 @@ public class LargestOfThreeNumbers {
         int num2 = 56;
         int num3 = 32;
 
-        if (num1 > num2 && num1 > num3) {
+        if (num1 >= num2 && num1 >= num3) {
             System.out.println("num1: " + num1 + " is largest number.");
-        } else if (num2 > num3) {
+        } else if (num2 >= num3) {
             System.out.println("num2: " + num2 + " is largest number.");
         } else {
             System.out.println("num3: " + num3 + " is largest number.");
