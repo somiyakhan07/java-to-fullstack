cb819444930a99a3eff84c4b3ab778a79400cbe5
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-06 23:06:22 +0530

    add IfElse_Statement/ with CheckOddEven Example in conditional statements

---
+++ JavaBasics/ConditionalStatements/IfElse_Statement/CheckOddEven.java
@@ -0,0 +1,15 @@
+// Check Odd Even
+
+public class CheckOddEven{
+    public static void main(String[] args){
+        int num = 14;
+
+        if(num%2 == 0){
+            System.out.println(num + " is a even number");
+        }
+        else{
+            System.out.println(num + " is a odd number");
+        }
+    }
+}
+// 14 is a even number
