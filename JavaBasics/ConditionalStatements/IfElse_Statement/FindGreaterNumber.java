f904fd0b10ad41501b41ede73711542ee3e34caa
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-06 23:13:52 +0530

    add FindGreaterNumber.java in if else statement

---
+++ JavaBasics/ConditionalStatements/IfElse_Statement/FindGreaterNumber.java
@@ -0,0 +1,18 @@
+// Find greater number
+
+public class FindGreaterNumber {
+    public static void main(String[] args){
+        int num1 = 21;
+        int num2 = 43;
+
+        if(num1 > num2){
+            System.out.println("num1: " + num1 + " is greater");
+        }
+        else{
+            System.out.println("num2: " + num2 + " is greater");
+        }
+
+    }
+    
+}
+// num2: 43 is greater
