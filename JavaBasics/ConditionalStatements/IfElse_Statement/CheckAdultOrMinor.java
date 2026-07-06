f9dbfd8caf5dec139f3ac37f8f8197424010069e
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-06 23:34:53 +0530

    add CheckAdultOrMinor.java in If Else Statement

---
+++ JavaBasics/ConditionalStatements/IfElse_Statement/CheckAdultOrMinor.java
@@ -0,0 +1,18 @@
+// Check Adult or Minor
+
+public class CheckAdultOrMinor {
+    public static void main(String[] args) {
+        String name = "Sanam";
+        int age = 21;
+
+        if (age >= 18) {
+            System.out.println(name + " is an adult");
+            System.out.println("Age: " + age);
+        } else {
+            System.out.println(name + " is an minor");
+            System.out.println("Age: " + age);
+        }
+    }
+}
+// Sanam is an adult
+// Age: 21
