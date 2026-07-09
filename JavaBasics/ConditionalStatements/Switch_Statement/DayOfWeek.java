ad28916f1da9d07b4a36c5497c26edf686cd4fa8
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-09 21:55:43 +0530

    add  Switch_Statement/ with Day Of Week examole in conditional statement

---
+++ JavaBasics/ConditionalStatements/Switch_Statement/DayOfWeek.java
@@ -0,0 +1,44 @@
+// print days of the week using switch
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+        int day = 6;
+
+        switch (day) {
+            case 1:
+                System.out.println("monday");
+                break;
+
+            case 2:
+                System.out.println("tuesday");
+                break;
+
+            case 3:
+                System.out.println("wednesday");
+                break;
+
+            case 4:
+                System.out.println("thursday");
+                break;
+
+            case 5:
+                System.out.println("friday");
+                break;
+
+            case 6:
+                System.out.println("saturday");
+                break;
+
+            case 7:
+                System.out.println("sunday");
+                break;
+
+            default:
+                System.out.println("Invalid Input");
+
+        }
+    }
+
+}
+
+// saturday
