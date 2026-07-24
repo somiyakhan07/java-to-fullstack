40e58bf11688417a3bc79eb0471c6257fe075f8d
somiyakhan07 <somiyakhan78600@gmail.com>
2026-07-24 19:15:35 +0530

    add GetterAndSetter.java in encapsulation

---
+++ OOPs/Encapsulation/GetterAndSetter.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Name {  
+    private String name;
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String n) {
+        name = n;
+    }
+}
+
+public class GetterAndSetter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Name obj = new Name();
+
+        System.out.print("Enter your name: ");
+        String inputName = sc.nextLine();
+        obj.setName(inputName);
+
+        System.out.println("name: " + obj.getName());
+    }
+}
