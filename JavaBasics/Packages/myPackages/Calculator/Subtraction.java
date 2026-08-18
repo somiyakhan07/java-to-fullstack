acbbc784d365f629962f2fdd58e849e724e04b7b
somiyakhan07 <somiyakhan78600@gmail.com>
2026-08-18 17:36:14 +0530

    add Calculator/ with Addition.java and Subtraction.java in myPackages

---
+++ JavaBasics/Packages/myPackages/Calculator/Subtraction.java
@@ -0,0 +1,30 @@
+// Create a user-defined package named calculator containing two classes: Addition and Subtraction. 
+// Create methods to perform addition and subtraction. Import these classes into another Java program and display the results.
+package myPackages.Calculator; // Define Calculator package
+
+public class Subtraction { // Subtraction class
+
+    private int n1; // First number
+    private int n2; // Second number
+
+    // Constructor to initialize numbers
+    public Subtraction(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    // Getter for first number
+    public int getN1() {
+        return n1;
+    }
+
+    // Getter for second number
+    public int getN2() {
+        return n2;
+    }
+
+    // Method to perform subtraction
+    public int sub() {
+        return n1 - n2;
+    }
+}
