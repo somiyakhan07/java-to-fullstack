4550ab04dae12d9cc8202819c701ee0a83387502
somiyakhan07 <somiyakhan78600@gmail.com>
2026-08-18 15:50:36 +0530

    add Packages/ with BuiltInPackages in java Basics

---
+++ JavaBasics/Packages/BuiltInPackages.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; // Built in java packages
+
+class Calc { // class created
+    public int add(int n1, int n2) { // add method
+        return n1 + n2;
+    }
+}
+
+public class BuiltInPackages { // main class
+    public static void main(String[] args) { // main method
+        Scanner sc = new Scanner(System.in); // Take user input
+
+        System.out.print("Enter number 1: "); // Take n1 from user 
+        int n1 = sc.nextInt(); // Scan n1 number
+
+        System.out.print("Enter number 2: "); // Take n2 from user 
+        int n2 = sc.nextInt(); // Scan n1 number
+
+        Calc c1 = new Calc(); // Calc class object created
+        int result = c1.add(n1, n2); // calls add method and put user value
+
+        System.out.println(n1 + " + " + n2 + " = " + result); // print result
+
+        sc.close();
+    }
+
+}
+/*
+output:
+Enter number 1: 10
+Enter number 2: 5
+10 + 5 = 15 
+*/
